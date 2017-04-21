24a70b0edc47508a5c95d1fd747b0ccb4ae7973f
Dag Ole Kristoffersen <thedole@gmail.com>
2017-04-21 23:54:49 +0200

    A bit more feedback about what is going on

--- MeditationtimerScratchpad/MainPage.xaml.cs
+++ MeditationtimerScratchpad/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Meditationtimer.Core;
 using Windows.UI.Core;
+using Windows.UI;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -31,17 +32,21 @@ namespace MeditationtimerScratchpad
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            var text = textBox.Text;
+            var text = input.Text;
             int seconds = 0;
             if (!Int32.TryParse(text, out seconds))
             {
+                input.BorderBrush = new SolidColorBrush(Color.FromArgb(128, 255, 0, 0));
                 output.Text = "Invalid input";
+                return;
             }
+
+            input.BorderBrush = new SolidColorBrush(Color.FromArgb(128, 0, 255, 0));
 
             var timer = new Timer(new TimeSpan(0, 0, seconds));
             timer.Completed += Timer_Completed;
             timer.Start();
-
+            output.Text = "Timer started";
         }
 
         async private void Timer_Completed(object sender)
